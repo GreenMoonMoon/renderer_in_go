97c44da12f3795504c05ff2444f1e9ca67a248ac
Joshua Boisvert <josue.boisvert@gmail.com>
2016-06-06 01:31:06 +0000

    deleted the imgio

--- src/imgio/imgio.go
+++
@@ -1,3 +0,0 @@
-package imgio
-
-/*Package created to handle images input and ouput functions.*/
--- src/rendering/canvas.go
+++
@@ -1,7 +0,0 @@
-package rendering
-
-import "geomath"
-
-func ProjectOnCanvas(v geomath.Vector) [2]float64 {
-    return [2]float64{v.X / -(v.Z), v.Y / -(v.Z)}
-}
